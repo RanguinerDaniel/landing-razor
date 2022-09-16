ba5fa1dafcc6ba39892dd3d03e67596dc6c24467
Ranguiner Daniel <103294791+RanguinerDaniel@users.noreply.github.com>
2022-09-15 23:14:58 -0400

    Update Script.js

--- project/assets/js/Script.js
+++ project/assets/js/Script.js
@@ -1,53 +1,70 @@
-function selected(id, state) {
-   var state = state;
-   if (state == true) {
-      showSelection(id);
 
-   } else {
-      hideSelection(id);
-      state = true;
+const virtuosSelectorOriginal = document.querySelector("#virtuos-selector").getAttribute("src");
+let virtuosSelector = document.querySelector("#virtuos-selector");
+
+virtuosSelector.addEventListener("click",function (e) {
+ let img = e.target.src.split('/');
+ let nomeImg = img[img.length - 1];
+
+   if(nomeImg == 'virtuos-select.png') {
+      virtuosSelector.setAttribute("src", virtuosSelectorOriginal);
+   } 
+
+   if(nomeImg == 'virtuos.jpg') {
+      virtuosSelector.setAttribute("src", "../project/assets/img/virtuos-select.png");
    }
 
-}
+});
 
-function hideSelection(id){
-   if (id == 1 || id == 2 || id == 3 || id == 4) {
-      if (id == 4) {
-         document.getElementById("virtuos-selector").src = "../project/assets/img/virtuos-select.png";
-      }
-      if (id == 3) {
-         document.getElementById("prodigy-selector").src = "../project/assets/img/prodigy-select.png";
-      }
-      if (id == 2) {
-         document.getElementById("talent-selector").src = "../project/assets/img/talent.jpg";
-      }
-      if (id == 1) {
-         document.getElementById("lean-selector").src = "../project/assets/img/lean-select.png";
-      }
-   } else {
-      alert("Desculpe! Houve um erro inesperado ao selecionar a máquina");
-      document.location.reload(true);
+
+const prodigySelectorOriginal = document.querySelector("#prodigy-selector").getAttribute("src");
+let prodigySelector = document.querySelector("#prodigy-selector");
+
+prodigySelector.addEventListener("click",function (e) {
+ let img = e.target.src.split('/');
+ let nomeImg = img[img.length - 1];
+
+   if(nomeImg == 'prodigy-select.png') {
+      prodigySelector.setAttribute("src", prodigySelectorOriginal);
+   } 
+
+   if(nomeImg == 'prodigy.jpg') {
+      prodigySelector.setAttribute("src", "../project/assets/img/prodigy-select.png");
    }
-}
+
+});
 
 
+const talentSelectorOriginal = document.querySelector("#talent-selector").getAttribute("src");
+let talentSelector = document.querySelector("#talent-selector");
 
-function showSelection(id) {
-   if (id == 1 || id == 2 || id == 3 || id == 4) {
-      if (id == 4) {
-         document.getElementById("virtuos-selector").src = "../project/assets/img/virtuos-select.png";
-      }
-      if (id == 3) {
-         document.getElementById("prodigy-selector").src = "../project/assets/img/prodigy-select.png";
-      }
-      if (id == 2) {
-         document.getElementById("talent-selector").src = "../project/assets/img/talent-select.png";
-      }
-      if (id == 1) {
-         document.getElementById("lean-selector").src = "../project/assets/img/lean-select.png";
-      }
-   } else {
-      alert("Desculpe! Houve um erro inesperado ao selecionar a máquina");
-      document.location.reload(true);
+talentSelector.addEventListener("click",function (e) {
+ let img = e.target.src.split('/');
+ let nomeImg = img[img.length - 1];
+
+   if(nomeImg == 'talent-select.png') {
+      talentSelector.setAttribute("src", talentSelectorOriginal);
+   } 
+
+   if(nomeImg == 'talent.jpg') {
+      talentSelector.setAttribute("src", "../project/assets/img/talent-select.png");
    }
-}
+
+});
+
+const leanSelectorOriginal = document.querySelector("#lean-selector").getAttribute("src");
+let leanSelector = document.querySelector("#lean-selector");
+
+leanSelector.addEventListener("click",function (e) {
+   let img = e.target.src.split('/');
+   let nomeImg = img[img.length - 1];
+   console.log(nomeImg);
+   if(nomeImg == 'lean-select.png') {
+      leanSelector.setAttribute("src", leanSelectorOriginal);
+   } 
+
+   if(nomeImg == 'lean.jpg') {
+      leanSelector.setAttribute("src", "../project/assets/img/lean-select.png");
+   }
+
+});
